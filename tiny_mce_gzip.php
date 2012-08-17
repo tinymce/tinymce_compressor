4f9e303a88035908ebd727adb425b6da75e2c405
Marcus <m.bleil@gmx.net>
2012-08-17 18:04:06 +0300

    Update tiny_mce_gzip.php
    I use contao CMS and TinyMCE in a SSL-Proxy environment. It mkaes my server accessable with SSL but sets the SSl-Proxy URL in front of the original server name. In this case TinyMCE will be loaded with wrong path and I can't use TinyMCE in SSL-Proxy environment. With the patch above it works. (see also https://github.com/contao/core/issues/3616#issuecomment-7636126)

--- tiny_mce_gzip.php
+++ tiny_mce_gzip.php
@@ -191,7 +191,7 @@ class TinyMCE_Compressor {
 		}
 
 		// Set base URL for where tinymce is loaded from
-		$buffer = "var tinyMCEPreInit={base:'" . dirname($_SERVER["SCRIPT_NAME"]) . "',suffix:''};";
+		$buffer = "var tinyMCEPreInit={base:'" . dirname($_SERVER['SERVER_NAME'] . $_SERVER["SCRIPT_NAME"]) . "',suffix:''};";
 
 		// Load all tinymce script files into buffer
 		foreach ($allFiles as $file) {
